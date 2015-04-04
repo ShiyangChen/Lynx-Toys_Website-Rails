216ae974ebd9a3c5e96155b2e0ece9ec1f0236e6
zhazheyu <zhazheyu@gmail.com>
2015-04-04 12:07:39 -0500

    upload multiple pictures, but related views need to be modified

--- lynxtoys/app/models/creation.rb
+++ lynxtoys/app/models/creation.rb
@@ -1,10 +1,10 @@
 class Creation<ActiveRecord::Base
-    #has_many :picture
+    has_many :pictures, :dependent=>:destroy
     #before_destroy :ensure_not_referenced_by_any_picture
 	
-    has_attached_file :image
+    #has_attached_file :image
     #has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	
     def ensure_not_referenced_by_any_pircture
         if pictures.count.zero?
--- lynxtoys/app/models/picture.rb
+++ lynxtoys/app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ActiveRecord::Base
-	belongs_to :creation, {:foreign_key => "creation_id"}  
+	belongs_to :creation  #, {:foreign_key => "creation_id"}  
+	has_attached_file :image, :path => ":rails_root/public/images/:id/:filename", :url  => "/images/:id/:filename"
+	do_not_validate_attachment_file_type :image
+
 	    
 end
