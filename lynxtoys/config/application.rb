dea6a22b6da14c1247ea00fcfab6d697a9fa42d3
ShiyangChen <chshysy910811@gmail.com>
2015-04-05 17:19:02 -0500

    deploy app to heroku; add config files

--- lynxtoys/config/application.rb
+++ lynxtoys/config/application.rb
@@ -22,6 +22,7 @@ module Lynxtoys
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+	config.serve_static_assets = true
 	config.generators do |g|
 		g.template_engine :haml
 	end
--- lynxtoys/features/ViewCreations.feature
+++
@@ -1,6 +0,0 @@
-Feature: User can view community collections
-
-Scenario: View a creation
-  Given I am on the home page
-  When I follow "Community Collections"
-  Then I should be on the Creations home page
