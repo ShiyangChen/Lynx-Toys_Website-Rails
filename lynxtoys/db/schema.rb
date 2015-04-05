e7eef2d80ae2c89412dd103158d0dc0deae37631
ShiyangChen <chshysy910811@gmail.com>
2015-04-05 15:17:27 -0500

    change database

--- lynxtoys/db/schema.rb
+++ lynxtoys/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150404163448) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "creations", force: :cascade do |t|
     t.string   "name"
     t.string   "creator_name"
