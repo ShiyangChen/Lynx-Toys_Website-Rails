89cc9175f0ae5620677a39dd9a1ec53c323a61b4
ShiyangChen <chshysy910811@gmail.com>
2015-04-18 12:29:00 -0500

    realized automatically sending notification email

--- lynxtoys/app/controllers/creations_controller.rb
+++ lynxtoys/app/controllers/creations_controller.rb
@@ -39,7 +39,7 @@ class CreationsController < ApplicationController
       	end
       end
     end
-	ManageMailer.sample_email.deliver
+	ManageMailer.sample_email(@creation).deliver
   end
   
   def show
--- lynxtoys/app/mailers/manage_mailer.rb
+++ lynxtoys/app/mailers/manage_mailer.rb
@@ -1,8 +1,8 @@
 class ManageMailer < ApplicationMailer
     default from: "lynxtoystest@gmail.com"
 
-    def sample_email
-        @receiver = "jf32799@tamu.edu"
-        mail(to: @receiver, subject: 'Sample Email')
+    def sample_email(creation)
+        @creation = creation
+        mail(:to => "lynxtoystest@gmail.com", :subject => 'Sample Email')
     end
 end
