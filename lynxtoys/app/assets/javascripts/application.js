0b304e3a86a9831d92317e660c08f34f703894f6
ShiyangChen <chshysy910811@gmail.com>
2015-04-25 19:56:07 -0300

    HHHH

--- lynxtoys/app/assets/javascripts/application.js
+++ lynxtoys/app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require fancybox
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  $("a.fancybox").fancybox();
+});
