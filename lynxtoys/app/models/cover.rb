d3ce1678fc6e6ddb2e0cd8d8453c269f9504df3d
ShiyangChen <chshysy910811@gmail.com>
2015-04-25 20:56:03 -0300

    Delete file in public

--- lynxtoys/app/models/cover.rb
+++ lynxtoys/app/models/cover.rb
@@ -1,5 +1,5 @@
 class Cover < ActiveRecord::Base
 	belongs_to :creation
-	has_attached_file :cover, :path => ":rails_root/public/covers/:id/:filename", :url  => "/covers/:id/:filename"
+	has_attached_file :cover, :path => ":rails_root/public/covers/:id_:filename", :url  => "/covers/:id_:filename"
 	do_not_validate_attachment_file_type :cover
 end
